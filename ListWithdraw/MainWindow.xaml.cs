45b63117ff464eacb71458f74a2f815dca0ecd34
Кирилл Лосев <grosfae@outlook.com>
2023-01-25 12:10:13 +0300

    Добавьте файлы проекта.

---
+++ ListWithdraw/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ListWithdraw.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListWithdraw
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LvBooks.ItemsSource = App.DB.Book.ToList();
+        }
+
+        private void ImageAddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedObject = LvBooks.SelectedItem as Book;
+            if (selectedObject == null)
+            {
+                MessageBox.Show("Выберите товар");
+                return;
+            }
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                selectedObject.Image = File.ReadAllBytes(dialog.FileName);
+                DataContext = null;
+                DataContext = selectedObject;
+                App.DB.SaveChanges();
+            }
+            
+        }
+    }
+}
---
+++ ListWithdraw/Model/PartialClass/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ListWithdraw.Model
+{
+    public partial class Book
+    {
+
+        public string Color
+        {
+            get
+            {
+                if (Discount > 15)
+                    return "#7fff00";
+                else
+                    return "Transparent";
+            }
+        }
+        public byte[] pictureEmpty
+        {
+            get
+            {
+                if (Image == null)
+                
+                return File.ReadAllBytes("C:\\Users\\grosf\\source\\repos\\ListWithdraw\\ListWithdraw\\Resources\\pictureEmp.png");
+
+                else
+                    return Image;
+            }
+        }
+        public Visibility VisibilityCost
+        {
+            get
+            {
+                if (Discount == null || Discount == 0)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+        public string StrCostDiscount
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return $"{Price} руб";
+                else
+                    return $"{(double)Price - ((double)Price / 100 * Discount)} руб";
+            }
+        }
+    }
+}
